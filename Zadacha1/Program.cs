fdbaf7b1e366cfadfd3463ef015fa650d94e5ab6
Temirlan Madykhan <tima.001@mail.ru>
2023-01-29 16:54:51 +0500

    HomeWork

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = 5;
+int b = 10;
+
+int max = a;
+if (b > max)  max = b;
+
+Console.WriteLine(max);
+
---
+++ Zadacha2/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = 234;
+int b = 1432;
+int c = 10000;
+
+int max = a;
+if (b > max)  max = b;
+if (c > max) max = c;
+
+Console.WriteLine(max);
